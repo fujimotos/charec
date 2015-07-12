d6baebc4c0fc380d3286e281bc3d2984664032a8
Fujimoto Seiji <fujimoto@ceptord.net>
2015-07-13 00:44:21 +0900

    Add a state machine to calculate the moving average.

---
+++ src/movingmean.ts
@@ -0,0 +1,38 @@
+module rechar {
+
+    /**
+     * A state machine to calculate the moving average.
+     */
+    export class MovingMean {
+        private buffer: Array<number>;
+        private total: number;
+        
+        constructor (public windowSize: number = 5) {
+            this.clear();
+        };
+
+        /** Reinitialize the internal state */
+        public clear(): void {
+            this.buffer = [];
+            this.total = 0;
+        };
+
+        /** Feed a value to the state machine */
+        public push(input: number): number {
+            this.buffer.push(input);
+
+            var winSize: number = this.windowSize;
+            var bufLen: number = this.buffer.length;
+
+            if (bufLen < winSize){
+                return null;
+            } else if (bufLen > winSize){
+                var shifted: number = this.buffer.shift();
+                this.total += input - shifted;
+            } else {
+                this.total = util.sum(this.buffer);
+            }
+            return this.total / winSize;
+        };
+    }
+}
