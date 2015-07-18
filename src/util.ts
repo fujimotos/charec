47a27cf9ea0e9d1e76b4ad337381059b6558b5bd
Fujimoto Seiji <fujimoto@ceptord.net>
2015-07-18 14:49:32 +0900

    Add Wagner-Fischre algorithm.

--- src/util.ts
+++ src/util.ts
@@ -16,4 +16,37 @@ module rechar.util {
     export function now(): number {
         return (new Date()).getTime();
     };
+
+    /**
+     * Wagner-Fisher algorithm written in TypeScript.
+     */
+    export function editdist(str1: string, str2: string){
+
+        var matrix: number[][] = [];
+        var i: number;
+        var j: number;
+
+        for (i = 0; i <= str1.length; i++){
+            matrix[i] = [i];
+        }
+        for (j = 0; j <= str2.length; j++){
+            matrix[0][j] = j;
+        }
+
+        for (j = 1; j <= str2.length; j++){
+            for (i = 1; i <= str1.length; i++){
+                if (str1.charAt(i-1) === str2.charAt(j-1)){
+                    matrix[i][j] = matrix[i-1][j-1];
+                } else {
+                    matrix[i][j] = Math.min(
+                        matrix[i-1][j],
+                        matrix[i][j-1],
+                        matrix[i-1][j-1]
+                    ) + 1;
+                }
+            }
+        }
+        return matrix[i-1][j-1];
+    }
+
 }
