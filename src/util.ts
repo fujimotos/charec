0c1bb7e10e360140dde78af00e52a850df35cd3f
Fujimoto Seiji <fujimoto@ceptord.net>
2015-07-25 14:27:40 +0900

    Restore the `util.now()` function.

--- src/util.ts
+++ src/util.ts
@@ -10,6 +10,13 @@ module charec.util {
     };
 
     /**
+     * Get the current time (in epoch).
+     */
+    export function now(): number {
+        return (new Date()).getTime();
+    };
+
+    /**
      * Wagner-Fischer algorithm implemented in TypeScript.
      */
     export function editdist(str1: string, str2: string){
